9566c3027de5ef656aa13bd3fa8e8187658953ec
dpx204710-crypto <dpx204710@gmail.com>
2025-11-02 14:22:16 +0800

    更新 next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,2 +1,6 @@
-const nextConfig = { reactStrictMode: true };
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
