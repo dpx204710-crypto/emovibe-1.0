ea4f0943f798b82965ce2a343ce46d7deb24da2c
dpx204710-crypto <dpx204710@gmail.com>
2025-11-09 04:17:51 +0800

    更新 next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
-  eslint: { ignoreDuringBuilds: true }
+  experimental: {
+    appDir: false
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com']
+  }
 };
-module.exports = nextConfig;
+
+export default nextConfig;
